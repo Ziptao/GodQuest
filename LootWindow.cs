3387310a25e63e1a8caf010e3eb9d1ff31dc132d
Ziptao <cburke345@yahoo.com>
2016-09-05 22:16:46 -0400

    new lootwindow files

---
+++ LootWindow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LootWindow : MonoBehaviour {
+
+    public GameObject loot_inventory_object;
+    
+
+    public void ActivateWindow()
+    {
+        loot_inventory_object.SetActive(!loot_inventory_object.activeSelf);
+    }    
+}
